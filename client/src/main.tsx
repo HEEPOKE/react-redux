81888beacb4c7c3d4bfb329914c8d42e40339c5c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-24 13:48:51 +0700

    :money_with_wings: Add React Bootstrap

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,6 @@
 import ReactDOM from "react-dom/client";
 import AppRouter from "./routes/AppRouter";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
