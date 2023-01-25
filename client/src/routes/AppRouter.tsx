ff4eed9fa81ee0d7f6d42be224566954495be4ae
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-25 09:08:57 +0700

    :money_with_wings: Create AuthPage

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -4,6 +4,8 @@ import { UserContextProvider } from "../contexts/UserContext";
 
 const HomePage = lazy(() => import("../pages/home"));
 const UserPage = lazy(() => import("../pages/user"));
+const LoginPage = lazy(() => import("../pages/auth/LoginPage"));
+const RegisterPage = lazy(() => import("../pages/auth/RegisterPage"));
 
 export default function AppRouter() {
   return (
@@ -11,6 +13,8 @@ export default function AppRouter() {
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<HomePage />} />
+          <Route path="/auth/login" element={<LoginPage />} />
+          <Route path="/auth/register" element={<RegisterPage />} />
           <Route
             path="/user"
             element={
