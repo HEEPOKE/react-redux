64a5879efef7f314a4619a52eb1cecdb0eef787c
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-25 16:35:11 +0700

    Update ValidateSwal.ts

--- client/src/utils/ValidateSwal.ts
+++ client/src/utils/ValidateSwal.ts
@@ -1,1 +1,17 @@
 import Swal from "sweetalert2";
+
+const ErrorMessage = (message: string) => {
+  Swal.fire({
+    icon: "error",
+    title: "กรอกข้อมูลผิดพลาด",
+    text: message,
+    showConfirmButton: false,
+    showDenyButton: true,
+    showCancelButton: true,
+    denyButtonText: "Close",
+  });
+};
+
+const ValidateUtils = { ErrorMessage };
+
+export default ValidateUtils;
