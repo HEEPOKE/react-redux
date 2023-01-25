312ecc752a14c26432609d41443debf327811a8d
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-25 11:25:14 +0700

    :money_with_wings: Edit file

--- client/src/common/ModalCommon.tsx
+++ client/src/common/ModalCommon.tsx
@@ -1,4 +1,4 @@
-import { Button, Modal } from "react-bootstrap";
+import { Modal } from "react-bootstrap";
 import ModalInterface from "../interface/ModalInterface";
 
 export default function ModalCommon({
