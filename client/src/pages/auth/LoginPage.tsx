f99ad10d6880d393890eb03e20e4ba030d49585d
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-25 09:43:02 +0700

    Update LoginPage.tsx

--- client/src/pages/auth/LoginPage.tsx
+++ client/src/pages/auth/LoginPage.tsx
@@ -1,9 +1,101 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Button, Form } from "react-bootstrap";
+import { LinkContainer } from "react-router-bootstrap";
 
 export default function LoginPage() {
+  const [validated, setValidated] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [remember, setRemember] = useState(false);
+
+  const Validation = (e: any) => {
+    const form = e.currentTarget;
+    if (form.checkValidity() === false) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    setValidated(true);
+  };
+
+  const handleSubmit = (e: any) => {
+    e.preventDefault();
+
+    var data = {
+      email: email,
+      password: password,
+    };
+
+    //   AuthApiServices.loginApi(data);
+  };
+
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="vh-100">
+      <div className="container-fluid h-custom">
+        <div className="row d-flex justify-content-center align-items-center h-100">
+          <div className="col-md-9 col-lg-6 col-xl-5">
+            <img
+              src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.svg"
+              className="img-fluid"
+              alt="Sample image"
+            />
+          </div>
+          <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
+            <div className="d-flex flex-row align-items-center justify-content-center justify-content-lg-start mb-3">
+              <h4>Login</h4>
+            </div>
+            <Form noValidate validated={validated} onSubmit={Validation}>
+              <Form.Group>
+                <Form.Label>Email</Form.Label>
+                <Form.Control
+                  type="text"
+                  onChange={(e: any) => setEmail(e.target.value)}
+                  placeholder="Enter Email"
+                  required
+                />
+              </Form.Group>
+              <Form.Group className="mb-3">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type="password"
+                  minLength={8}
+                  onChange={(e: any) => setPassword(e.target.value)}
+                  placeholder="Enter Password"
+                  required
+                />
+              </Form.Group>
+              <Form.Group
+                className="d-flex justify-content-between align-items-center"
+                controlId="formBasicCheckbox"
+              >
+                <Form.Check type="checkbox" label="Remember me" />
+                <LinkContainer to="/auth/Forgot-password">
+                  <a>Forgot password?</a>
+                </LinkContainer>
+              </Form.Group>
+              <Button
+                className="btn-lg mt-2 col-12"
+                variant="primary"
+                onClick={handleSubmit}
+                type="submit"
+              >
+                Login
+              </Button>
+            </Form>
+            <div className="text-center text-lg-start">
+              <p className="small fw-bold mt-2 pt-1 mb-0">
+                Don't have an account?{" "}
+                <LinkContainer to="/auth/register">
+                  <a className="link-danger NN">Register</a>
+                </LinkContainer>
+              </p>
+            </div>
+            <div className="text-center mt-2 fw-bold">
+              <h4>OR</h4>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
