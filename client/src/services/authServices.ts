da0152198158ad81a198719d79b953ca3c2458bf
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-25 13:56:04 +0700

    Update authServices.ts

--- client/src/services/authServices.ts
+++ client/src/services/authServices.ts
@@ -4,6 +4,10 @@ const login = (payload: any) => {
   return http.post(`api/auth/login`, payload);
 };
 
-const authServices = { login };
+const register = (payload: any) => {
+  return http.post(`api/auth/register`, payload);
+};
+
+const authServices = { login, register };
 
 export default authServices;
