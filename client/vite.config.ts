a7484e6c3beee3c9e54ba0dd2c756cbf5fede1d1
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-30 17:09:06 +0700

    :money_with_wings: Set unit test

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import EnvironmentPlugin from "vite-plugin-environment";
 
 export default defineConfig({
   plugins: [react(), EnvironmentPlugin("all", { defineOn: "import.meta.env" })],
+  // test: {
+  //   globals: true,
+  //   environment: "jsdom",
+  //   setupFiles: "./test/setup.ts",
+  // },
   define: {
     global: {},
   },
