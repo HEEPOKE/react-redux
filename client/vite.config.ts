855fc88452ef2103b9d567d39773333bfe57b2c2
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-31 11:59:04 +0700

    :money_with_wings: Update Vitest

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import EnvironmentPlugin from "vite-plugin-environment";
@@ -6,5 +9,17 @@ export default defineConfig({
   plugins: [react(), EnvironmentPlugin("all", { defineOn: "import.meta.env" })],
   define: {
     global: {},
+  },
+  test: {
+    globals: true,
+    // environment: "jsdom",
+    environment: 'happy-dom',
+    setupFiles: "./test/setup.ts",
+    css: true,
+    reporters: ["default", "html"],
+    coverage: {
+      provider: "istanbul",
+      reporter: ['text', 'json', 'html'],
+    },
   },
 });
