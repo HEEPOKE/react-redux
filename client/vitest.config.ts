0cc3785d7b82232f46aa5cdd1676c813d12c6eac
Poundpitak Amornlerdlittichai <Damon1FX@gmail.com>
2023-01-31 11:56:34 +0700

    Update vitest.config.ts

--- client/vitest.config.ts
+++ client/vitest.config.ts
@@ -3,7 +3,8 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globals: true,
-    environment: "jsdom",
+    // environment: "jsdom",
+    environment: 'happy-dom',
     setupFiles: "./test/setup.ts",
     css: true,
     reporters: ["default", "html"],
